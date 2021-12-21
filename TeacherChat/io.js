9d2f79185486fa4ae1444dc037af5984d5d35868
Dor Zairi <dorzairi@ymail.com>
2021-12-21 15:13:22 -0500

    added the socket.io to the express project and able to send messages to the console

--- TeacherChat/io.js
+++ TeacherChat/io.js
@@ -1,10 +1,19 @@
 // io.js
+const colors = require("colors");
 
 var io = require("socket.io")();
 
 // Listen for new connections from clients (socket)
 io.on("connection", function (socket) {
-  console.log("Client connected to socket.io!");
+  console.log("<socket.io> Client connected to socket.io!".green);
+
+  socket.on("chat message", (msg) => {
+    console.log(`message: ${msg}`.italic.bgBlack.cyan);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("<socket.io> Client Discoonnected from socket.io!".yellow);
+  });
 });
 
 // io represents socket.io on the server - let's export it
